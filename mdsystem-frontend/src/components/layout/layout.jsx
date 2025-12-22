80ce65fde0c557f11d9f4e307affd604dc86fbda
K1taru <K1taru@users.noreply.github.com>
2025-12-22 11:50:16 +0800

    reset layout imports

--- mdsystem-frontend/src/components/layout/layout.jsx
+++ mdsystem-frontend/src/components/layout/layout.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Sidebar from './sidebar_temp';
+import Sidebar from './sidebasar_temp';
 import TopBar from './top-bar';
 
 const Layout = ({ children }) => {
