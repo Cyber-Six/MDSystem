2159332ef691389d05cc94725c42819758329b0b
K1taru <K1taru@users.noreply.github.com>
2026-04-06 23:33:36 +0800

    revert to using     WHERE ps."slotEntityId" = $1

--- Backend/routes/appointment/resolvers/wrapper/helper.js
+++ Backend/routes/appointment/resolvers/wrapper/helper.js
@@ -249,7 +249,7 @@ async function getPatientIdFromSlotId(slotId) {
   const query = `
     SELECT ps."patientId"
     FROM "patientSlot" ps
-    WHERE ps.id = $1
+    WHERE ps."slotEntityId" = $1
     LIMIT 1;
   `;
 
