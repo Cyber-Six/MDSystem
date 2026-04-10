8394981a468d26522b62ef79b4747968566a055d
John Rhey Pena <jrhey.pena@gmail.com>
2026-04-10 20:06:43 +0800

    fix failing adjust/split batch

--- Backend/routes/medical-inventory/inventory/resolvers/wrapper/helper.js
+++ Backend/routes/medical-inventory/inventory/resolvers/wrapper/helper.js
@@ -26,7 +26,7 @@ async function batchIdToBranch(batch, batchId) {
   }
 
   const result = await db.query(
-    `SELECT branch FROM "${batch}" WHERE id = $1 LIMIT 1`,
+    `SELECT location FROM "${batch}" WHERE id = $1 LIMIT 1`,
     [batchId]
   );
   if (result.rows.length === 0) {
